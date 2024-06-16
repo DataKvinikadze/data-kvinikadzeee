414e0b42aa00b03b7090821d4a7aee14e1687a8b
DataKvinikadze <datakvinikadze07@gmail.com>
2024-06-16 20:22:12 +0400

    .

--- script/script.js
+++ script/script.js
@@ -25,10 +25,19 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 3,
 });
 
+// Date difference
+
+let days = document.querySelector(".dayDif");
+let hours = document.querySelector(".hourDif");
+let minutes = document.querySelector(".minutDif");
+let seconds = document.querySelector(".secondDif");
+console.log(days);
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
 // saboloo ricxvi
 
 let endDate = new Date("2024-06-30 23:59:59");
-
 // vigeb mocanemebs
 let EndMonth = endDate.getMonth() + 1;
 let endHours = endDate.getHours();
@@ -36,37 +45,30 @@ let endMinutes = endDate.getMinutes();
 let endSeconds = endDate.getSeconds();
 let endDayOfMonth = endDate.getDate();
 
-// axlandeli ricxvi
-let currentTime = new Date();
+setInterval(() => {
+  // axlandeli ricxvi
+  let currentTime = new Date();
 
-// vigeb mocanemebs
-let currentMonth = currentTime.getMonth() + 1;
-let currentHours = currentTime.getHours();
-let currentMinutes = currentTime.getMinutes();
-let currentSeconds = currentTime.getSeconds();
-let currentDayOfMonth = currentTime.getDate();
+  // vigeb mocanemebs
+  let currentMonth = currentTime.getMonth() + 1;
+  let currentHours = currentTime.getHours();
+  let currentMinutes = currentTime.getMinutes();
+  let currentSeconds = currentTime.getSeconds();
+  let currentDayOfMonth = currentTime.getDate();
 
-// vaconsoleb
-console.log(
-  `Current Time: ${currentDayOfMonth} ${currentHours}:${currentMinutes}:${currentSeconds}`
-);
-console.log(
-  `end Time: ${endDayOfMonth} ${endHours}:${endMinutes}:${endSeconds}`
-);
-
-// gansxvavebebs vigeb
-let differenceInDay = endDayOfMonth - currentDayOfMonth;
-console.log(differenceInDay);
-let differenceInHours = endHours - currentHours;
-console.log(differenceInHours);
-let differenceInMinutes = endMinutes - currentMinutes;
-console.log(differenceInMinutes);
-let differenceInSeconds = endSeconds - currentSeconds;
-console.log(differenceInSeconds);
-let differenceInMonths = EndMonth - currentMonth;
-console.log(differenceInMonths);
-
-// aq tu tveebshi moxda cvlileba differenceInDays emateba gasuli tvis dgeebis raodenoba
-if (differenceInMonths > 0) {
-  differenceInDay = differenceInDay + 30 * differenceInMonths;
-}
+  // gansxvavebebs vigeb
+  let differenceInDay = endDayOfMonth - currentDayOfMonth;
+  days.textContent = differenceInDay;
+  let differenceInHours = endHours - currentHours;
+  hours.textContent = differenceInHours;
+  let differenceInMinutes = endMinutes - currentMinutes;
+  minutes.textContent = differenceInMinutes;
+  let differenceInSeconds = endSeconds - currentSeconds;
+  seconds.textContent = differenceInSeconds;
+  let differenceInMonths = EndMonth - currentMonth;
+  console.log(differenceInMonths);
+  // aq tu tveebshi moxda cvlileba differenceInDays emateba gasuli tvis dgeebis raodenoba
+  if (differenceInMonths > 0) {
+    differenceInDay = differenceInDay + 30 * differenceInMonths;
+  }
+}, 1000);
